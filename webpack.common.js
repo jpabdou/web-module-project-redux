e243d2eecc640296a661e856e922eaa7dfc3cbb4
jpabdou <johnpabdou@gmail.com>
2022-12-09 21:07:17 -0800

    updated with deployment setup

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,48 @@
-const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-
-  entry: {
-
-    app: './src/index.js',
-
-  },
-
-  plugins: [
-
-    new HtmlWebpackPlugin({
-
-      title: 'Production',
-
-    }),
-
-  ],
-
+  entry: './src/index.js',
+  mode: 'development',
   output: {
-
     filename: '[name].bundle.js',
-
     path: path.resolve(__dirname, 'dist'),
-
     clean: true,
-
   },
-
-};
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        exclude: /node_modules/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            'presets': [['@babel/preset-env', { targets: { chrome: '80' } }]],
+            'plugins': [['babel-plugin-styled-components'], ['@babel/plugin-transform-react-jsx']]
+          }
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+}
